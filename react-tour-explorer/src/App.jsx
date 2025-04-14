d402b1e64e5eae44e94f753d4309ba504bea5109
tracyn09 <tracyn0099@gmail.com>
2025-04-14 13:42:59 -0400

    Reset View– Refresh Button

--- react-tour-explorer/src/App.jsx
+++ react-tour-explorer/src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import DestinationSelector from './DestinationSelector' //passing selected destinination
 import Gallery from './Gallery'
 
+
+
 //useState to store tours, pass loading, and error
 function App() {
   const [tours, setTours] = useState([])      
@@ -9,18 +11,27 @@ function App() {
   const [error, setError] = useState(null)   
   const [selectedDestination, setSelectedDestination] = useState('All')  
 
+  //remove tour
   const handleRemove = (id) => {
-    setTours(tours.filter((tour) => tour.id !== id));
-  };
+    setTours(tours.filter((tour) => tour.id !== id))
+  }
   
+  //refresh tours
+  const handleRefresh = () => {
+    setRefresh(!refresh)
+  }
+  
+  const [refresh, setRefresh] = useState(false)
 
 
 //Fetch tours from API using useEffect
   useEffect(() => {
+    setLoading(true)
     fetch('https://cors-anywhere.herokuapp.com/https://course-api.com/react-tours-project')
       .then((res) => res.json())
       .then((data) => {
         setTours(data)
+        setErrors(null)
         setLoading(false)
       })
 
@@ -29,8 +40,8 @@ function App() {
         setError('Error detected')
         setLoading(false)
       })
-  }, [])
-
+  }, [refresh])
+  
 
   return (
     <div>
@@ -41,7 +52,12 @@ function App() {
       {!loading && <DestinationSelector tours={tours} selected={selectedDestination} setSelected={setSelectedDestination} />}
       
       <Gallery tours={tours} selected={selectedDestination} onRemove={handleRemove}/>
-
+      {!loading && tours.length === 0 && (
+    <div>
+      <p>No tours left. Refresh to reload.</p>
+      <button onClick={handleRefresh}>Refresh</button> 
+    </div>
+)}
     </div>
   )
 }
