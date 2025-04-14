62b3a50fd665c16bcce5087415aea9051f0f5bd3
tracyn09 <tracyn0099@gmail.com>
2025-04-14 13:15:55 -0400

    Fetch & Store Tour Data

--- react-tour-explorer/src/App.jsx
+++ react-tour-explorer/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
+
+//useState to store tours, pass loading, and error
 function App() {
-  const [count, setCount] = useState(0)
+  const [tours, setTours] = useState([])      
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)   
+ 
+
+
+//Fetch tours from API using useEffect
+  useEffect(() => {
+    fetch('https://cors-anywhere.herokuapp.com/https://course-api.com/react-tours-project')
+      .then((res) => res.json())
+      .then((data) => {
+        setTours(data)
+        setLoading(false)
+      })
+
+
+      .catch(() => {
+        setError('Error detected')
+        setLoading(false)
+      })
+  }, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Available Tours</h1>
+      {loading && <p>Loading tours...</p>}
+      {error && <p>{error}</p>}
+      {!loading && <p>{tours.length} tours available.</p>}
+
+
+    </div>
   )
 }
 
